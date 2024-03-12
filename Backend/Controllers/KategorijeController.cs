44cb97281fe26945ddd4a38c87830a092714dedb
njakopec <153132341+njakopec@users.noreply.github.com>
2024-03-12 16:25:06 +0100

    Kreiranje Kategorije CRUD

---
+++ Backend/Controllers/KategorijeController.cs
@@ -0,0 +1,64 @@
+﻿using EdunovaAPP.Data;
+using EdunovaAPP.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EdunovaAPP.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class KategorijeController
+    {
+        // Dependency injection
+        // Definiraš privatno svojstvo
+        private readonly EdunovaContext _context;
+
+        // Dependency injection
+        // U konstruktoru primir instancu i dodjeliš privatnom svojstvu
+        public KategorijeController(EdunovaContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new JsonResult(_context.Kategorije.ToList());
+        }
+
+        [HttpPost]
+        public IActionResult Post(Kategorija kategorija)
+        {
+            _context.Kategorije.Add(kategorija);
+            _context.SaveChanges();
+            return new JsonResult(kategorija);
+        }
+
+        [HttpPut]
+        [Route("{sifra:int}")]
+        public IActionResult Put(int sifra, Kategorija kategorija)
+        {
+            var kategorijaIzBaze = _context.Kategorije.Find(sifra);
+            // za sada ručno, kasnije će doći Mapper
+            kategorijaIzBaze.Naziv = kategorija.Naziv;
+            kategorijaIzBaze.VrijediOd= kategorija.VrijediOd;
+
+            _context.Kategorije.Update(kategorijaIzBaze);
+            _context.SaveChanges();
+
+            return new JsonResult(kategorijaIzBaze);
+        }
+
+        [HttpDelete]
+        [Route("{sifra:int}")]
+        [Produces("application/json")]
+        public IActionResult Delete(int sifra)
+        {
+            var smjerIzBaze = _context.Kategorije.Find(sifra);
+            _context.Kategorije.Remove(smjerIzBaze);
+            _context.SaveChanges();
+            return new JsonResult(new { poruka="Obrisano"});
+        }
+
+    }
+}
---
+++ Backend/Models/Kategorija.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EdunovaAPP.Models
+{
+    public class Kategorija: Entitet
+    {
+        public string? Naziv { get; set; }
+        public DateTime? VrijediOd { get; set; }
+
+    }
+}
